ksimon1/kubevirt-tekton-tasks-operator

Initial commit which adds basic skeleton for operator
This PR add basic skeleton for kubevirt-tekton-tasks-operator.
Purpose of this operator is to deploy and take care of https://github.com/kubevirt/kubevirt-tekton-tasks
and example pipelines. Operator will be deployed as a part of HCO.
Operator will not be deployed by default. Users will have to set a
special bool attribute in HCO CR (e.g. deployTektonOperator). Deployment
of operator will be possible even after installation of cnv. In case a
user would like to use an operator, he/she has to install tekton
pipelines / openshift pipelines into cluster. If tekton is not
installed, the operator will be deployed and will be waiting for
installation of tekton and there will be a message of required crds in
the operator log. After tekton is installed, the operator will deploy
tasks and pipelines.

Signed-off-by: Karel Šimon <ksimon@redhat.com>

pkg/tekton-bundle/tekton-bundle.go
@@ -1,0 +1,199 @@
+package tekton_bundle
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/kubevirt/kubevirt-tekton-tasks-operator/pkg/operands"
+	openshiftconfigv1 "github.com/openshift/api/config/v1"
+	pipeline "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	yamlv2 "gopkg.in/yaml.v2"
+	v1 "k8s.io/api/core/v1"
+	rbac "k8s.io/api/rbac/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	tektonTasksKubernetesBundleDir     = "/data/tekton-tasks/kubernetes/"
+	tektonTasksOKDBundleDir            = "/data/tekton-tasks/okd/"
+	tektonPipelinesKubernetesBundleDir = "/data/tekton-pipelines/kubernetes/"
+	tektonPipelinesOKDBundleDir        = "/data/tekton-pipelines/okd/"
+)
+
+var (
+	clusterTasksString = string(pipeline.ClusterTaskKind)
+	pipelineKindString = "Pipeline"
+	serviceAccountKind = rbac.ServiceAccountKind
+	roleBindingKind    = "RoleBinding"
+	clusterRoleKind    = "ClusterRole"
+	configMapKind      = "ConfigMap"
+)
+
+type Bundle struct {
+	ClusterTasks    []pipeline.ClusterTask
+	ServiceAccounts []v1.ServiceAccount
+	RoleBindings    []rbac.RoleBinding
+	ClusterRoles    []rbac.ClusterRole
+	Pipelines       []pipeline.Pipeline
+	ConfigMaps      []v1.ConfigMap
+}
+
+func ReadBundle(cl client.Reader, ctx context.Context) (*Bundle, error) {
+	isOpenshift, err := runningOnOpenshift(cl, ctx)
+	if err != nil {
+		return nil, err
+	}
+	taskPath := getTasksBundlePath(isOpenshift)
+
+	pipelinePath := getPipelineBundlePath(isOpenshift)
+
+	tektonObjs, err := decodeObjectsFromFiles(taskPath, pipelinePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return tektonObjs, nil
+}
+
+func getPipelineBundlePath(isOpenshift bool) string {
+	if isOpenshift {
+		return tektonPipelinesOKDBundleDir
+	}
+	return tektonPipelinesKubernetesBundleDir
+}
+
+func getTasksBundlePath(isOpenshift bool) string {
+	if isOpenshift {
+		return filepath.Join(tektonTasksOKDBundleDir, "kubevirt-tekton-tasks-okd-"+operands.Version+".yaml")
+	}
+	return filepath.Join(tektonTasksKubernetesBundleDir, "kubevirt-tekton-tasks-kubernetes-"+operands.Version+".yaml")
+}
+
+func runningOnOpenshift(cl client.Reader, ctx context.Context) (bool, error) {
+	clusterVersion := &openshiftconfigv1.ClusterVersion{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "version",
+		},
+	}
+	if err := cl.Get(ctx, client.ObjectKeyFromObject(clusterVersion), clusterVersion); err != nil {
+		if meta.IsNoMatchError(err) || apierrors.IsNotFound(err) {
+			// Not on OpenShift
+			return false, nil
+		} else {
+			return false, err
+		}
+	}
+	return true, nil
+}
+
+func readFile(fileName string) ([]byte, error) {
+	return ioutil.ReadFile(fileName)
+}
+
+func readFolder(folderPath string) ([][]byte, error) {
+	files, err := ioutil.ReadDir(folderPath)
+	if err != nil {
+		return nil, err
+	}
+	filesBytes := make([][]byte, len(files))
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		f, err := ioutil.ReadFile(filepath.Join(folderPath, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+		filesBytes = append(filesBytes, f)
+	}
+
+	return filesBytes, nil
+}
+
+func readFiles(taskFilename, pipelinesFolder string) ([][]byte, error) {
+	files := [][]byte{}
+	fileBytes, err := ioutil.ReadFile(taskFilename)
+	if err != nil {
+		return nil, err
+	}
+
+	files = append(files, fileBytes)
+
+	filesBytes, err := readFolder(pipelinesFolder)
+	if err != nil {
+		return nil, err
+	}
+
+	return append(files, filesBytes...), nil
+}
+
+func decodeObjectsFromFiles(taskFilename, pipelinesFolder string) (*Bundle, error) {
+	files, err := readFiles(taskFilename, pipelinesFolder)
+	if err != nil {
+		return nil, err
+	}
+
+	bundle := &Bundle{}
+	for _, file := range files {
+		decoder := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(file), 1024)
+		for {
+			var obj map[string]interface{}
+			err = decoder.Decode(&obj)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return nil, err
+			}
+
+			if kind, ok := obj["kind"].(string); ok {
+				switch kind {
+				case clusterTasksString:
+					clusterTask := pipeline.ClusterTask{}
+					err = getObject(obj, &clusterTask)
+					bundle.ClusterTasks = append(bundle.ClusterTasks, clusterTask)
+				case pipelineKindString:
+					p := pipeline.Pipeline{}
+					err = getObject(obj, &p)
+					bundle.Pipelines = append(bundle.Pipelines, p)
+				case serviceAccountKind:
+					sa := v1.ServiceAccount{}
+					err = getObject(obj, &sa)
+					bundle.ServiceAccounts = append(bundle.ServiceAccounts, sa)
+				case roleBindingKind:
+					rb := rbac.RoleBinding{}
+					err = getObject(obj, &rb)
+					bundle.RoleBindings = append(bundle.RoleBindings, rb)
+				case clusterRoleKind:
+					cr := rbac.ClusterRole{}
+					err = getObject(obj, &cr)
+					bundle.ClusterRoles = append(bundle.ClusterRoles, cr)
+				case configMapKind:
+					cm := v1.ConfigMap{}
+					err = getObject(obj, &cm)
+					bundle.ConfigMaps = append(bundle.ConfigMaps, cm)
+				default:
+					continue
+				}
+			}
+		}
+	}
+	return bundle, nil
+}
+
+func getObject(obj map[string]interface{}, newObj interface{}) error {
+	o, err := yamlv2.Marshal(&obj)
+	if err != nil {
+		return err
+	}
+
+	return yaml.Unmarshal(o, newObj)
+}

kubevirt/tekton-tasks-operator

pkg/tekton-bundle/tekton-bundle.go
@@ -1,0 +1,202 @@
+package tekton_bundle
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/kubevirt/tekton-tasks-operator/pkg/operands"
+	openshiftconfigv1 "github.com/openshift/api/config/v1"
+	pipeline "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	yamlv2 "gopkg.in/yaml.v2"
+	v1 "k8s.io/api/core/v1"
+	rbac "k8s.io/api/rbac/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	tektonTasksKubernetesBundleDir     = "/data/tekton-tasks/kubernetes/"
+	tektonTasksOKDBundleDir            = "/data/tekton-tasks/okd/"
+	tektonPipelinesKubernetesBundleDir = "/data/tekton-pipelines/kubernetes/"
+	tektonPipelinesOKDBundleDir        = "/data/tekton-pipelines/okd/"
+)
+
+var (
+	clusterTasksString = string(pipeline.ClusterTaskKind)
+	pipelineKindString = "Pipeline"
+	serviceAccountKind = rbac.ServiceAccountKind
+	roleBindingKind    = "RoleBinding"
+	clusterRoleKind    = "ClusterRole"
+	configMapKind      = "ConfigMap"
+)
+
+type Bundle struct {
+	ClusterTasks    []pipeline.ClusterTask
+	ServiceAccounts []v1.ServiceAccount
+	RoleBindings    []rbac.RoleBinding
+	ClusterRoles    []rbac.ClusterRole
+	Pipelines       []pipeline.Pipeline
+	ConfigMaps      []v1.ConfigMap
+}
+
+func ReadBundle(cl client.Reader, ctx context.Context) (*Bundle, error) {
+	isOpenshift, err := runningOnOpenshift(cl, ctx)
+	if err != nil {
+		return nil, err
+	}
+	taskPath := getTasksBundlePath(isOpenshift)
+
+	pipelinePath := getPipelineBundlePath(isOpenshift)
+
+	tektonObjs, err := decodeObjectsFromFiles(taskPath, pipelinePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return tektonObjs, nil
+}
+
+func getPipelineBundlePath(isOpenshift bool) string {
+	if isOpenshift {
+		return tektonPipelinesOKDBundleDir
+	}
+	return tektonPipelinesKubernetesBundleDir
+}
+
+func getTasksBundlePath(isOpenshift bool) string {
+	if isOpenshift {
+		return filepath.Join(tektonTasksOKDBundleDir, "kubevirt-tekton-tasks-okd-"+operands.TektonTasksVersion+".yaml")
+	}
+	return filepath.Join(tektonTasksKubernetesBundleDir, "kubevirt-tekton-tasks-kubernetes-"+operands.TektonTasksVersion+".yaml")
+}
+
+func runningOnOpenshift(cl client.Reader, ctx context.Context) (bool, error) {
+	clusterVersion := &openshiftconfigv1.ClusterVersion{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "version",
+		},
+	}
+	if err := cl.Get(ctx, client.ObjectKeyFromObject(clusterVersion), clusterVersion); err != nil {
+		if meta.IsNoMatchError(err) || apierrors.IsNotFound(err) {
+			// Not on OpenShift
+			return false, nil
+		} else {
+			return false, err
+		}
+	}
+	return true, nil
+}
+
+func readFile(fileName string) ([]byte, error) {
+	return ioutil.ReadFile(fileName)
+}
+
+func readFolder(folderPath string) ([][]byte, error) {
+	files, err := ioutil.ReadDir(folderPath)
+	if err != nil {
+		return nil, err
+	}
+	filesBytes := make([][]byte, len(files))
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		f, err := ioutil.ReadFile(filepath.Join(folderPath, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+		filesBytes = append(filesBytes, f)
+	}
+
+	return filesBytes, nil
+}
+
+func readFiles(taskFilename, pipelinesFolder string) ([][]byte, error) {
+	files := [][]byte{}
+	fileBytes, err := ioutil.ReadFile(taskFilename)
+	if err != nil {
+		return nil, err
+	}
+
+	files = append(files, fileBytes)
+
+	filesBytes, err := readFolder(pipelinesFolder)
+	if err != nil {
+		return nil, err
+	}
+
+	return append(files, filesBytes...), nil
+}
+
+func decodeObjectsFromFiles(taskFilename, pipelinesFolder string) (*Bundle, error) {
+	files, err := readFiles(taskFilename, pipelinesFolder)
+	if err != nil {
+		return nil, err
+	}
+
+	bundle := &Bundle{}
+	for _, file := range files {
+		decoder := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(file), 1024)
+		for {
+			var obj map[string]interface{}
+			err = decoder.Decode(&obj)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return nil, err
+			}
+			if kind, ok := obj["kind"].(string); ok {
+				if kind == "" {
+					continue
+				}
+
+				switch kind {
+				case clusterTasksString:
+					clusterTask := pipeline.ClusterTask{}
+					err = getObject(obj, &clusterTask)
+					bundle.ClusterTasks = append(bundle.ClusterTasks, clusterTask)
+				case pipelineKindString:
+					p := pipeline.Pipeline{}
+					err = getObject(obj, &p)
+					bundle.Pipelines = append(bundle.Pipelines, p)
+				case serviceAccountKind:
+					sa := v1.ServiceAccount{}
+					err = getObject(obj, &sa)
+					bundle.ServiceAccounts = append(bundle.ServiceAccounts, sa)
+				case roleBindingKind:
+					rb := rbac.RoleBinding{}
+					err = getObject(obj, &rb)
+					bundle.RoleBindings = append(bundle.RoleBindings, rb)
+				case clusterRoleKind:
+					cr := rbac.ClusterRole{}
+					err = getObject(obj, &cr)
+					bundle.ClusterRoles = append(bundle.ClusterRoles, cr)
+				case configMapKind:
+					cm := v1.ConfigMap{}
+					err = getObject(obj, &cm)
+					bundle.ConfigMaps = append(bundle.ConfigMaps, cm)
+				default:
+					continue
+				}
+			}
+		}
+	}
+	return bundle, nil
+}
+
+func getObject(obj map[string]interface{}, newObj interface{}) error {
+	o, err := yamlv2.Marshal(&obj)
+	if err != nil {
+		return err
+	}
+
+	return yaml.Unmarshal(o, newObj)
+}