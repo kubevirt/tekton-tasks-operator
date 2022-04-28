ab915f77eb52fbd27623c974711933c03a2e7547
Karel Šimon <ksimon@redhat.com>
2022-04-28 14:34:42 +0200

    remove commands, which add priviledges to tekton fix rollout command for tekton
    Signed-off-by: Karel Šimon <ksimon@redhat.com>

--- scripts/deploy-resources.sh
+++ scripts/deploy-resources.sh
@@ -6,17 +6,14 @@ if oc get namespace tekton-pipelines > /dev/null 2>&1; then
   exit 0
 fi
 
-KUBEVIRT_VERSION=$(curl -s https://github.com/kubevirt/kubevirt/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
-CDI_VERSION=$(curl -s https://github.com/kubevirt/containerized-data-importer/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
+KUBEVIRT_VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases | \
+            jq '.[] | select(.prerelease==false) | .tag_name' | sort -V | tail -n1 | tr -d '"')
+
+CDI_VERSION=$(curl -s https://api.github.com/repos/kubevirt/containerized-data-importer/releases | \
+            jq '.[] | select(.prerelease==false) | .tag_name' | sort -V | tail -n1 | tr -d '"')
 
 TEKTON_VERSION=$(curl -s https://api.github.com/repos/tektoncd/operator/releases | \
             jq '.[] | select(.prerelease==false) | .tag_name' | sort -V | tail -n1 | tr -d '"')
-
-if oc get templates > /dev/null 2>&1; then
-  # Prepare Tekton Pipelines
-  oc adm policy add-scc-to-user anyuid -z tekton-pipelines-controller
-  oc adm policy add-scc-to-user anyuid -z tekton-pipelines-webhook
-fi
 
 # Deploy Tekton Pipelines
 oc apply -f "https://github.com/tektoncd/operator/releases/download/${TEKTON_VERSION}/openshift-release.yaml"
@@ -34,8 +31,7 @@ oc apply -f "https://github.com/kubevirt/containerized-data-importer/releases/do
 oc apply -f "https://github.com/kubevirt/containerized-data-importer/releases/download/${CDI_VERSION}/cdi-cr.yaml"
 
 # wait for tekton pipelines
-oc rollout status -n openshift-pipelines deployment/tekton-pipelines-controller --timeout 10m
-oc rollout status -n openshift-pipelines deployment/tekton-pipelines-webhook --timeout 10m
+oc rollout status -n openshift-operators deployment/openshift-pipelines-operator --timeout 10m
 
 # Wait for kubevirt to be available
 oc rollout status -n cdi deployment/cdi-operator --timeout 10m
