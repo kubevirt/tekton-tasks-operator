c891a1784da50035d5f3bcafbbfbbebcc97d6ad7
Karel Šimon <ksimon@redhat.com>
2023-02-16 11:26:26 +0100

    fix: revert kubevirt version back to v0.58.0
    Kubevirt v0.58.1 and 0.59.0-rc.0 probably contains bug, which is causing
    that windows vm is in pending state. The virt-launcher is in unschedulable
    state (probably due to TSC node selector). Test lanes, which are testing
    example pipelines are failing due to that issue. Reverting kubevirt
    version back will fix the test lanes, until the issue is fixed in
    kubevirt project.
    Signed-off-by: Karel Šimon <ksimon@redhat.com>

--- scripts/deploy-resources.sh
+++ scripts/deploy-resources.sh
@@ -6,8 +6,9 @@ if oc get namespace tekton-pipelines > /dev/null 2>&1; then
   exit 0
 fi
 
-KUBEVIRT_VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases | \
-            jq '.[] | select(.prerelease==false) | .tag_name' | sort -V | tail -n1 | tr -d '"')
+KUBEVIRT_VERSION="v0.58.0"
+#$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases | \
+#           jq '.[] | select(.prerelease==false) | .tag_name' | sort -V | tail -n1 | tr -d '"')
 
 CDI_VERSION=$(curl -s https://api.github.com/repos/kubevirt/containerized-data-importer/releases | \
             jq '.[] | select(.prerelease==false) | .tag_name' | sort -V | tail -n1 | tr -d '"')
